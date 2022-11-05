1d4bd0b7596c038a675828a52944e64d1c9928b9
ThanachaiJrw <103188624+ThanachaiJrw@users.noreply.github.com>
2022-11-05 19:11:27 +0700

    add e-comerce

--- src/page/Shop.js
+++ src/page/Shop.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState ,useEffect } from 'react'
 import Products from '../components/E-commerce/Products'
 
+import { commerce } from '../utils/commerce'
+
 const Shop = () => {
+  const [products,setProducts] = useState([])
+
+  const fetchProducts =async()=>{
+
+    const {data} =await commerce.products.list();
+
+    setProducts({data})
+  }
+
+  useEffect(()=>{
+    fetchProducts();
+  },[]);
+
   return (
     <div>
-      <Products/>
+      <Products />
     </div>
   )
 }
